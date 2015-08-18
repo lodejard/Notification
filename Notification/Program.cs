bca3b71bc0e3db2f186cb9487157c731c7927bcb
Louis DeJardin <lodejard@microsoft.com>
2015-08-17 18:51:40 -0700

    Change to support instantiatable hubs
    * TelemetryHub can be instantiated
    * Cross-talk with default hub established by default
    * example of child hubs, child hub notification, and child hub
      subscription added to Program.cs

--- Notification/Program.cs
+++ Notification/Program.cs
@@ -88,33 +88,51 @@ namespace System.Diagnostics.Tracing
 
             // To set up a sink for notifications you implement an INotifier and subscribe to the hub you want
             // as long as the subscription is not disposed, the INotifier will get callbacks.  
-            using (var subscription = TelemetryHub.DefaultDispatcher.Subscribe(new MyNotificationReceiver()))
+            using (var subscription = TelemetryHub.DefaultDispatcher.Subscribe(new MyNotificationReceiver("Global")))
             {
-                // Here we simulate what might happen in the class library where we don't use dependency injection.
-                // You can also get you iNotifier by asking IServiceProvider which might make one per tenant.  
-                ITelemetryNotifier iNotifier = TelemetryHub.DefaultNotifier;
-
-                // Normally this would be in code that was receiving the HttpRequestResponse
-                HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "http://localhost/");
-
-                // Here we log for simple cases as show below we don't need to do the ShuldNotify, but we are
-                // showing the general case where there might be significant work setting up the payload. 
-                if (iNotifier.ShouldNotify("OutgoingHttpRequestReturns"))
+                // This simulates an application that is creating Dispatcher/Notifier pairs for instantiable in-process contexts.
+                // The child hubs each cross-talk with the default hub, but do not cross-talk with each other
+                using (var childHub1 = new TelemetryHub())
+                using (var childHub2 = new TelemetryHub())
+                using (childHub1.Dispatcher.Subscribe(new MyNotificationReceiver("Child1")))
+                using (childHub2.Dispatcher.Subscribe(new MyNotificationReceiver("Child2")))
                 {
-                    // Here we are assuming we would like to log both to EventSource and direct subscribers to
-                    // NotificationHub.   Because of this the payload class contains both serializable fields (like 
-                    // ReqeustUri which we resolve to a string), as well as rich objects (like Message) that are
-                    // stripped from EventSource serialization.  
-                    iNotifier.Notify("OutgoingHttpRequestReturns", new { RequestUri = message.RequestUri.ToString(), Message = message });
+                    // Here we simulate what might happen in the class library where we don't use dependency injection.
+                    // You can also get you iNotifier by asking IServiceProvider which might make one per tenant.  
+                    ITelemetryNotifier iNotifier = TelemetryHub.DefaultNotifier;
+
+                    // Normally this would be in code that was receiving the HttpRequestResponse
+                    HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "http://localhost/");
+
+                    // Here we log for simple cases as show below we don't need to do the ShuldNotify, but we are
+                    // showing the general case where there might be significant work setting up the payload. 
+                    if (iNotifier.ShouldNotify("OutgoingHttpRequestReturns"))
+                    {
+                        // Here we are assuming we would like to log both to EventSource and direct subscribers to
+                        // NotificationHub.   Because of this the payload class contains both serializable fields (like 
+                        // ReqeustUri which we resolve to a string), as well as rich objects (like Message) that are
+                        // stripped from EventSource serialization.  
+                        iNotifier.Notify("OutgoingHttpRequestReturns", new { RequestUri = message.RequestUri.ToString(), Message = message });
+                    }
+
+                    childHub1.Notifier.Notify("TalkingToChildHub", "Child Hub One");
+                    childHub2.Notifier.Notify("TalkingToChildHub", "Child Hub Two");
                 }
             }
         }
 
         class MyNotificationReceiver : ITelemetryNotifier
         {
+            string m_zone;
+
+            public MyNotificationReceiver(string zone)
+            {
+                m_zone = zone;
+            }
+
             public void Notify(string notificationName, object parameters)
             {
-                Console.WriteLine("Got a notification {0} with parameters of type {1}", notificationName, parameters);
+                Console.WriteLine("{2} got a notification {0} with parameters of type {1}", notificationName, parameters, m_zone);
             }
 
             public bool ShouldNotify(string notificationName)
--- System.Notification/TelemetryHub.cs
+++ System.Notification/TelemetryHub.cs
@@ -13,10 +13,8 @@ namespace System.Notification
     /// implemented INotifiy) can be hooked up.  
     /// </summary>
     /// TODO: What IObserviable support should we add if any (it could be added later)
-    public class TelemetryHub : ITelemetryDispatcher, ITelemetryNotifier
-    { 
-        private static TelemetryHub Default = new TelemetryHub();
-
+    public class TelemetryHub : IDisposable
+    {
         /// <summary>
         /// This is the notification hub that is used by default by the class library.   
         /// Generally you don't want to make your own but rather have everyone use this one, which
@@ -24,14 +22,13 @@ namespace System.Notification
         /// The main reason not to us this one is that you WANT isolation from other 
         /// events in the system (e.g. multi-tenancy).  
         /// </summary>
-        public static ITelemetryDispatcher DefaultDispatcher => Default.Dispatcher;
+        public static ITelemetryDispatcher DefaultDispatcher => s_default.Dispatcher;
 
-        public static ITelemetryNotifier DefaultNotifier => Default.Notifier;
+        public static ITelemetryNotifier DefaultNotifier => s_default.Notifier;
 
+        public ITelemetryDispatcher Dispatcher => m_dispatcher;
 
-        public ITelemetryDispatcher Dispatcher { get { return this; } }
-
-        public ITelemetryNotifier Notifier { get { return this; } }
+        public ITelemetryNotifier Notifier => m_notifier;
 
         /// <summary>
         /// Make a new notifier, it is a INotifier, which means the returned result can be used to 
@@ -40,76 +37,140 @@ namespace System.Notification
         /// (multi-casting).    Generally you should not be making your own notifier but use the
         /// Notifier.DefaultNotifier, so that notifications are as 'public' as possible.  
         /// </summary>
-        public TelemetryHub()
-        { }
-
-        // INotfier implementation
-        bool ITelemetryNotifier.ShouldNotify(string notificationName)
+        public TelemetryHub() : this(connectToDefault: true)
         {
-            for (var curSubscription = m_subscriptions; curSubscription != null; curSubscription = curSubscription.Next)
+        }
+
+        public TelemetryHub(bool connectToDefault)
+        {
+            m_dispatcher = new HubDispatcher();
+            m_notifier = new HubNotifier(m_dispatcher);
+
+            if (connectToDefault)
             {
-                if (curSubscription.Subscriber.ShouldNotify(notificationName))
-                    return true;
+                var dispatcherFromDefaultNotifier = (ITelemetryDispatcher)s_default.Notifier;
+                var notifierFromDefaultDispatcher = (ITelemetryNotifier)s_default.Dispatcher;
+                var dispatcherFromThisNotifier = (ITelemetryDispatcher)Notifier;
+                var notifierFromThisDispatcher = (ITelemetryNotifier)Dispatcher;
+
+                m_subscriptionFromDefaultNotifier = dispatcherFromDefaultNotifier.Subscribe(notifierFromThisDispatcher);
+                m_subscriptionToDefaultDispatcher = dispatcherFromThisNotifier.Subscribe(notifierFromDefaultDispatcher);
             }
-            return false;
         }
 
-        void ITelemetryNotifier.Notify(string notificationName, object parameters)
+        public void Dispose()
         {
-            for (var curSubscription = m_subscriptions; curSubscription != null; curSubscription = curSubscription.Next)
-                curSubscription.Subscriber.Notify(notificationName, parameters);
+            m_subscriptionFromDefaultNotifier?.Dispose();
+            m_subscriptionToDefaultDispatcher?.Dispose();
         }
 
-        // Subscription implementation 
-        /// <summary>
-        /// Add a subscriber, Dispose the returned value to unsubscribe.  Every
-        /// subscribers will have their ShouldNotify and Notify methods called
-        /// whenever a producer is needs to cause a notification.  
-        /// </summary>
-        IDisposable ITelemetryDispatcher.Subscribe(ITelemetryNotifier subscriber)
-        {
-            Subscription newSubscription = new Subscription() { Subscriber = subscriber, Owner = this, Next = m_subscriptions };
-            while (Interlocked.CompareExchange(ref m_subscriptions, newSubscription, newSubscription.Next) != newSubscription.Next)
-                newSubscription.Next = m_subscriptions;
-            return newSubscription;
-        }
 
         #region private
 
-        // Note that Subscriptions are READ ONLY.   This means you never update any fields (even on removal!)
-        private class Subscription : IDisposable
+        private class HubDispatcher : ITelemetryDispatcher, ITelemetryNotifier
         {
-            internal ITelemetryNotifier Subscriber;
-            internal TelemetryHub Owner;       // The hub this is a subscription for.  
-            internal Subscription Next;           // Linked list
+            Subscription m_subscriptions; // A linked list of subsciptions   Note this is ENTIRELY (DEEP) read only.   
 
-            public void Dispose()
+            // INotfier implementation
+            public virtual bool ShouldNotify(string notificationName)
             {
-                // TO keep this lock free and easy to analyze, the linked list is READ ONLY.   Thus we copy
-
-                for (;;)
+                for (var curSubscription = m_subscriptions; curSubscription != null; curSubscription = curSubscription.Next)
                 {
-                    Subscription subscriptions = Owner.m_subscriptions;
-                    Subscription newSubscriptions = Remove(subscriptions, this);    // Make a new list, with myself removed.  
-
-                    // try to update, but if someone beat us to it, then retry.  
-                    if (Interlocked.CompareExchange(ref Owner.m_subscriptions, newSubscriptions, subscriptions) == newSubscriptions)
-                        break;
+                    if (curSubscription.Subscriber.ShouldNotify(notificationName))
+                        return true;
                 }
+                return false;
             }
 
-            // Create a new linked list where 'subscription has been removed from the linked list of 'subscriptions'. 
-            private static Subscription Remove(Subscription subscriptions, Subscription subscription)
+            public virtual void Notify(string notificationName, object parameters)
             {
-                if (subscriptions == null)
-                    return null;
-                if (subscriptions == subscription)
-                    return subscriptions.Next;
-                return new Subscription() { Subscriber = subscriptions.Subscriber, Owner = subscriptions.Owner, Next = Remove(subscriptions.Next, subscription) };
+                for (var curSubscription = m_subscriptions; curSubscription != null; curSubscription = curSubscription.Next)
+                    curSubscription.Subscriber.Notify(notificationName, parameters);
+            }
+
+            // Subscription implementation 
+            /// <summary>
+            /// Add a subscriber, Dispose the returned value to unsubscribe.  Every
+            /// subscribers will have their ShouldNotify and Notify methods called
+            /// whenever a producer is needs to cause a notification.  
+            /// </summary>
+            IDisposable ITelemetryDispatcher.Subscribe(ITelemetryNotifier subscriber)
+            {
+                Subscription newSubscription = new Subscription() { Subscriber = subscriber, Owner = this, Next = m_subscriptions };
+                while (Interlocked.CompareExchange(ref m_subscriptions, newSubscription, newSubscription.Next) != newSubscription.Next)
+                    newSubscription.Next = m_subscriptions;
+                return newSubscription;
+            }
+
+            // Note that Subscriptions are READ ONLY.   This means you never update any fields (even on removal!)
+            private class Subscription : IDisposable
+            {
+                internal ITelemetryNotifier Subscriber;
+                internal HubDispatcher Owner;       // The hub this is a subscription for.  
+                internal Subscription Next;           // Linked list
+
+                public void Dispose()
+                {
+                    // TO keep this lock free and easy to analyze, the linked list is READ ONLY.   Thus we copy
+
+                    for (;;)
+                    {
+                        Subscription subscriptions = Owner.m_subscriptions;
+                        Subscription newSubscriptions = Remove(subscriptions, this);    // Make a new list, with myself removed.  
+
+                        // try to update, but if someone beat us to it, then retry.  
+                        if (Interlocked.CompareExchange(ref Owner.m_subscriptions, newSubscriptions, subscriptions) == newSubscriptions)
+                            break;
+                    }
+                }
+
+                // Create a new linked list where 'subscription has been removed from the linked list of 'subscriptions'. 
+                private static Subscription Remove(Subscription subscriptions, Subscription subscription)
+                {
+                    if (subscriptions == null)
+                        return null;
+                    if (subscriptions == subscription)
+                        return subscriptions.Next;
+                    return new Subscription() { Subscriber = subscriptions.Subscriber, Owner = subscriptions.Owner, Next = Remove(subscriptions.Next, subscription) };
+                }
             }
         }
 
-        Subscription m_subscriptions; // A linked list of subsciptions   Note this is ENTIRELY (DEEP) read only.   
+        private class HubNotifier : HubDispatcher
+        {
+            public HubNotifier(HubDispatcher dispatcher)
+            {
+                m_dispatcher = dispatcher;
+            }
+
+            #region private
+            public override bool ShouldNotify(string notificationName)
+            {
+                if (m_dispatcher.ShouldNotify(notificationName))
+                {
+                    return true;
+                }
+                return base.ShouldNotify(notificationName);
+            }
+
+            public override void Notify(string notificationName, object parameters)
+            {
+                m_dispatcher.Notify(notificationName, parameters);
+                base.Notify(notificationName, parameters);
+            }
+
+            HubDispatcher m_dispatcher;
+            #endregion
+        }
+
+
+        static TelemetryHub s_default = new TelemetryHub(connectToDefault: false);
+
+        HubNotifier m_notifier;
+        HubDispatcher m_dispatcher;
+        IDisposable m_subscriptionFromDefaultNotifier;
+        IDisposable m_subscriptionToDefaultDispatcher;
+
         #endregion
     }
 
